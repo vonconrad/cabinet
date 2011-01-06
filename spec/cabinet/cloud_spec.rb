a244f3aba0cf4cbbcbbd9dfffdda3fda92bd9786
vonconrad <sebastian@e-channel.com.au>
2011-01-06 10:56:05 +1030

    Move file name and content specification to spec_helper

--- spec/cabinet/cloud_spec.rb
+++ spec/cabinet/cloud_spec.rb
@@ -4,7 +4,5 @@ describe Cabinet::Cloud do
   before(:all) do
     credentials   = YAML.load_file(File.expand_path(File.join(File.dirname(__FILE__), '..', 'cloud_credentials.yml')))
     @cc           = Cabinet::Cloud.new({:container => 'cabinet_test'}.merge(credentials))
-    @file_name    = 'cabinet.test'
-    @file_content = (0...50).map{('a'..'z').to_a[rand(26)]}.join
   end
 end
--- spec/spec_helper.rb
+++ spec/spec_helper.rb
@@ -1,1 +1,8 @@
 require File.join(File.dirname(__FILE__), '..', 'lib', 'cabinet')
+
+RSpec.configure do |config|
+  config.before(:suite) do
+    @@file_name    = 'cabinet.test'
+    @@file_content = (0...50).map{('a'..'z').to_a[rand(26)]}.join
+  end
+end
