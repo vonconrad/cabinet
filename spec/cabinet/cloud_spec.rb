69e94d969106710f7c46cfcd24ec3a5c1cd36fcc
vonconrad <sebastian@e-channel.com.au>
2011-01-06 10:56:27 +1030

    Add basic tests for cloud spec

--- spec/cabinet/cloud_spec.rb
+++ spec/cabinet/cloud_spec.rb
@@ -5,4 +5,35 @@ describe Cabinet::Cloud do
     credentials   = YAML.load_file(File.expand_path(File.join(File.dirname(__FILE__), '..', 'cloud_credentials.yml')))
     @cc           = Cabinet::Cloud.new({:container => 'cabinet_test'}.merge(credentials))
   end
+
+  it "should create file" do
+    @cc.put(@@file_name, @@file_content).should == true
+  end
+
+  it "should confirm file exists" do
+    @cc.exists?(@@file_name).should == true
+  end
+
+  it "should confirm file does not exist" do
+    @cc.exists?(@@file_content).should == false
+  end
+
+  it "should read file" do
+    @cc.get(@@file_name).should == @@file_content
+  end
+
+  it "should list files"
+
+  it "should not overwrite file"
+
+  it "should overwrite file if :force => true"
+
+  it "should gzip file"
+
+  it "should delete file" do
+    @cc.delete(@@file_name)
+    @cc.exists?(@@file_name).should == false
+  end
+
+  it "should bulk delete files"
 end
