7d887f1afc3d48f973b6ff51f049920b2bebc7cd
vonconrad <sebastian@e-channel.com.au>
2011-01-06 10:56:59 +1030

    Trim whitespace

--- spec/cabinet/cloud_spec.rb
+++ spec/cabinet/cloud_spec.rb
@@ -2,8 +2,8 @@ require File.expand_path(File.join(File.dirname(__FILE__), '..', 'spec_helper'))
 
 describe Cabinet::Cloud do
   before(:all) do
-    credentials   = YAML.load_file(File.expand_path(File.join(File.dirname(__FILE__), '..', 'cloud_credentials.yml')))
-    @cc           = Cabinet::Cloud.new({:container => 'cabinet_test'}.merge(credentials))
+    credentials = YAML.load_file(File.expand_path(File.join(File.dirname(__FILE__), '..', 'cloud_credentials.yml')))
+    @cc         = Cabinet::Cloud.new({:container => 'cabinet_test'}.merge(credentials))
   end
 
   it "should create file" do
