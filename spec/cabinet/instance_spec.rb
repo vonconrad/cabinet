88300acfe7bd4b395ef0a98686e7518ce2402d6b
vonconrad <sebastian@e-channel.com.au>
2011-06-05 16:07:48 +0930

    Rewrite test titles

--- spec/cabinet/instance_spec.rb
+++ spec/cabinet/instance_spec.rb
@@ -18,33 +18,33 @@ describe Cabinet::Instance do
     @local.delete(/.*/)
   end
 
-  it "should create file" do
+  it "creates files" do
     @local.put(@file_name, @file_content).should == true
   end
 
-  it "should confirm file exists" do
+  it "confirms that files exists" do
     @local.exists?(@file_name).should == true
   end
 
-  it "should confirm file does not exist" do
+  it "confirms that files don't exist" do
     @local.exists?(@file_content).should == false
   end
 
-  it "should read file" do
+  it "reads file content" do
     @local.get(@file_name).should == @file_content
   end
 
-  it "should get last modified" do
+  it "fetches last modified timestamp" do
     @local.modified(@file_name).class.should == Time
   end
 
-  it "should append file" do
+  it "appends files" do
     extra_content = Forgery(:lorem_ipsum).text(:paragraph)
     @local.append(@file_name, extra_content).should == true
     @local.get(@file_name).should == @file_content + extra_content
   end
 
-  it "should list files" do
+  it "list files with (and without) regular expressions" do
     @local.list.should include(@file_name)
     @local.list(/#{@file_name}/).should include(@file_name)
     @local.list(/#{@file_content}/).should == []
@@ -97,12 +97,12 @@ describe Cabinet::Instance do
     @local.decompress(gz_file_name).should == @file_content
   end
 
-  it "should delete file" do
+  it "deletes files" do
     @local.delete(@file_name).should  == true
     @local.exists?(@file_name).should == false
   end
   
-  it "should bulk delete files" do
+  it "bulk deletes files using regular expressions" do
     (1..3).each {|n| @local.put("#{@file_name}.#{n}", @file_content)}
     @local.delete(/#{@file_name}/)
     (1..3).inject([]) {|arr, n| arr << @local.exists?("#{@file_name}.#{n}")}.should == [false, false, false]
